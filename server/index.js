523b028787d423c4e084b1e0a770c8db2eaef35c
toll anbesu <tolaanbesu@gmail.com>
2025-11-07 21:19:40 +0300

    add generated prisma client

--- server/index.js
+++ server/index.js
@@ -10,8 +10,10 @@ import swapRoutes from './routes/swapRoutes.js';
 
 const app = express();
 
+app.use(cors())
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://slot-swapper-frontend-five.vercel.app',
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(express.json());
